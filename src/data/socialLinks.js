d6cfeb9e133bf02ee5ddfc0c9e84bcf29bad8062
yyusronwirawan <yusronwirawan@yahoo.com>
2023-08-24 00:30:03 +0700

    first commit

---
+++ src/data/socialLinks.js
@@ -0,0 +1,32 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: 'https://www.yusron.biz.id/',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/yyusronwirawan',
+	},
+	{
+		id: 3,
+		name: 'Twitter',
+		icon: 'twitter',
+		url: 'https://twitter.com/yyusronwirawan',
+	},
+	{
+		id: 4,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/yyusronwirawan',
+	},
+	{
+		id: 5,
+		name: 'YouTube',
+		icon: 'youtube',
+		url: 'https://www.youtube.com/c/yyusronwirawan',
+	},
+];
